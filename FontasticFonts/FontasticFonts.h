671d11f067cd89ef1761221e97459539b0dad01c
jungwook <jungwook.hahn@me.com>
2018-06-13 08:54:18 +0900

    first

---
+++ FontasticFonts/FontasticFonts.h
@@ -0,0 +1,19 @@
+//
+//  FontasticFonts.h
+//  FontasticFonts
+//
+//  Created by 한정욱 on 2018. 6. 13..
+//  Copyright © 2018년 한정욱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FontasticFonts.
+FOUNDATION_EXPORT double FontasticFontsVersionNumber;
+
+//! Project version string for FontasticFonts.
+FOUNDATION_EXPORT const unsigned char FontasticFontsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FontasticFonts/PublicHeader.h>
+
+
